d1689b204b9f7d5a618d0c037de92fdd851ed4ba
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-26 00:07:17 +0700

    add npm modules
    - learn about validator npm package

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,5 +1,10 @@
-const getNotes = require('./notes')
-console.log(getNotes())
+const validator = require('validator');
+const getNotes = require('./notes');
+
+console.log(getNotes());
+
+console.log(validator.isEmail('theerapon.nanoi.185@gmail.com'));
+console.log(validator.isURL('theerapon.com'));
 
 // const add = require('./utils')
 // const sum = add(4, -2)
