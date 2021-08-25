d0cb79e8da939488614f2defde7d9fa2a328349d
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-26 00:18:45 +0700

    add npm chalk
    - change color massage

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const validator = require('validator');
 const getNotes = require('./notes');
 
@@ -5,6 +6,9 @@ console.log(getNotes());
 
 console.log(validator.isEmail('theerapon.nanoi.185@gmail.com'));
 console.log(validator.isURL('theerapon.com'));
+console.log(chalk.green('Success!'))
+console.log(chalk.bold.green('Success!'))
+console.log(chalk.bold.green.inverse('Success!'));
 
 // const add = require('./utils')
 // const sum = add(4, -2)
