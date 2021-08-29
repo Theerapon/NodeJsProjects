7e7d82e0eb384dbfb64a1dba819258afa9cfe519
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-29 17:06:46 +0700

    Add Web-Dynamic with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,66 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const geocode = require('../../Weather-App/utils/geocode')
+const forecast = require('../../Weather-App/utils/forecast')
+
 const app = express()
 
-// app.com/
-app.get('', (request, response) => {
-    response.send('Hellfo express!')
+// Define paths for Express config
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Fakie Home'
+    })
 })
 
-// app.com/help
-app.get('/help', (request, response) => {
-    response.send('Help page')
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Fakie About'
+    })
 })
 
-// app.com/about
-app.get('/about', (request, response) => {
-    response.send('About page')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Theerapon Help'
+    })
 })
 
 // app.com/weather
 app.get('/weather', (request, response) => {
-    response.send('Weather page')
+    geocode('Boston', (error, {latitude, longitude, location}) => {
+        if (error) {
+            return response.send('Error: ' + error)
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return response.send('Error: ' + error)
+            }
+            
+            response.send({
+                location: location,
+                forecastData: forecastData,
+            })
+        })
+    })
+    
 })
 
 
